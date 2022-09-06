f163a8c8ba9cedb209d0309cd3c00f65bb34a610
hungVIetTa <viethunglpt@gmail.com>
2022-09-06 23:14:04 +0700

    fghg

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,17 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <SignUp/>
 </template>
+
+<script>
+import SignUp from './components/SignUp.vue'
+
+export default {
+  name: 'App',
+  components: {
+    SignUp
+  }
+}
+</script>
 
 <style>
 #app {
